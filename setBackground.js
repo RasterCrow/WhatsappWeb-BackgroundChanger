2d9fc90a8948a7a34e22c3428dc754ab8a9d22d0
Alex Andries <rastercrow@gmail.com>
2020-11-26 19:58:37 +0100

    fix for latest update

--- setBackground.js
+++ setBackground.js
@@ -6,7 +6,8 @@ chrome.storage.sync.get("backgroundImage", function (data) {
         backgroundImage = data.backgroundImage;
 });
 
-$(document).arrive("._2-aNW", function () {
+//_2-aNW
+$(document).arrive(".RUGMB", function () {
     // 'this' refers to the newly created element
     var $newElem = $(this);
     if (backgroundImage !== "") {
