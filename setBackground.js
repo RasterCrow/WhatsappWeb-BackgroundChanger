9707247f95dec06043c22c583eb04e1ef1ba7524
Alex Andries <rastercrow@gmail.com>
2021-07-09 10:41:49 +0200

    fixed for new update of whatsapp web

--- setBackground.js
+++ setBackground.js
@@ -7,7 +7,9 @@ chrome.storage.sync.get("backgroundImage", function (data) {
 });
 
 //_2-aNW
-$(document).arrive(".RUGMB", function () {
+//.RUGMB
+//._2wjK5
+$(document).arrive("._1LcQK", function () {
     // 'this' refers to the newly created element
     var $newElem = $(this);
     if (backgroundImage !== "") {
