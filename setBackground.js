4a9ec6cdfcdb22d2506f1df9623949ade0afc1a1
Alex Andries <rastercrow@gmail.com>
2022-04-15 12:00:30 +0200

    updated to latest whatsapp web version

--- setBackground.js
+++ setBackground.js
@@ -1,20 +1,24 @@
-
-let backgroundImage = ""
+let backgroundImage = "";
 chrome.storage.sync.get("backgroundImage", function (data) {
-    console.log(data.backgroundImage)
-    if (data.backgroundImage !== undefined)
-        backgroundImage = data.backgroundImage;
+  console.log(data.backgroundImage);
+  if (data.backgroundImage !== undefined)
+    backgroundImage = data.backgroundImage;
 });
 
 //_2-aNW
 //.RUGMB
 //._2wjK5
-$(document).arrive("._1LcQK", function () {
-    // 'this' refers to the newly created element
-    var $newElem = $(this);
-    if (backgroundImage !== "") {
-        $newElem.css("backgroundImage", "url(" + backgroundImage + ")", "!important")
-        $newElem.css("background-size", "cover")
-    }
-});
+//._1LcQK
 
+$(document).arrive("._2gzeB", function () {
+  // 'this' refers to the newly created element
+  var $newElem = $(this);
+  if (backgroundImage !== "") {
+    $newElem.css(
+      "backgroundImage",
+      "url(" + backgroundImage + ")",
+      "!important"
+    );
+    $newElem.css("background-size", "cover");
+  }
+});
